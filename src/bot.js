3b2e911bdfc485bb170f983f23664df0187b1b0f
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-11-11 15:44:14 +0530

    starting

--- src/bot.js
+++ src/bot.js
@@ -7,100 +7,113 @@ let bot = new Bot()
 // ROUTING
 
 bot.onEvent = function(session, message) {
-  switch (message.type) {
-    case 'Init':
-      welcome(session)
-      break
-    case 'Message':
-      onMessage(session, message)
-      break
-    case 'Command':
-      onCommand(session, message)
-      break
-    case 'Payment':
-      onPayment(session, message)
-      break
-    case 'PaymentRequest':
-      welcome(session)
-      break
-  }
+    switch (message.type) {
+        case 'Init':
+            welcome(session)
+            break
+        case 'Message':
+            onMessage(session, message)
+            break
+        case 'Command':
+            onCommand(session, message)
+            break
+        case 'Payment':
+            onPayment(session, message)
+            break
+        case 'PaymentRequest':
+            welcome(session)
+            break
+    }
 }
 
 function onMessage(session, message) {
-  welcome(session)
+    handleOnMessage(session,message)
 }
-
+function handleOnMessage(session, message) {
+    sendMessage(session,'I am sorry i was not able to understand you i am still under development')
+}
 function onCommand(session, command) {
-  switch (command.content.value) {
-    case 'ping':
-      pong(session)
-      break
-    case 'count':
-      count(session)
-      break
-    case 'donate':
-      donate(session)
-      break
+    switch (command.content.value) {
+        case 'currentBills':
+            currentBills(session)
+            break
+        case 'count':
+            count(session)
+            break
+        case 'donate':
+            donate(session)
+            break
+        case 'showValues':
+            showValues(session)
+            break
     }
 }
 
 function onPayment(session, message) {
-  if (message.fromAddress == session.config.paymentAddress) {
-    // handle payments sent by the bot
-    if (message.status == 'confirmed') {
-      // perform special action once the payment has been confirmed
-      // on the network
-    } else if (message.status == 'error') {
-      // oops, something went wrong with a payment we tried to send!
+    if (message.fromAddress == session.config.paymentAddress) {
+        // handle payments sent by the bot
+        if (message.status == 'confirmed') {
+            // perform special action once the payment has been confirmed
+            // on the network
+        } else if (message.status == 'error') {
+            // oops, something went wrong with a payment we tried to send!
+        }
+    } else {
+        // handle payments sent to the bot
+        if (message.status == 'unconfirmed') {
+            // payment has been sent to the ethereum network, but is not yet confirmed
+            sendMessage(session, `Thanks for the payment! 🙏`);
+        } else if (message.status == 'confirmed') {
+            // handle when the payment is actually confirmed!
+        } else if (message.status == 'error') {
+            sendMessage(session, `There was an error with your payment!🚫`);
+        }
     }
-  } else {
-    // handle payments sent to the bot
-    if (message.status == 'unconfirmed') {
-      // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
-    } else if (message.status == 'confirmed') {
-      // handle when the payment is actually confirmed!
-    } else if (message.status == 'error') {
-      sendMessage(session, `There was an error with your payment!🚫`);
-    }
-  }
 }
 
 // STATES
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
+    sendMessage(session, `Hey there !`)
 }
 
 function pong(session) {
-  sendMessage(session, `Pong`)
+    sendMessage(session, `Pong`)
 }
-
+function showValues(session) {
+    sendMessage(session,'')
+}
 // example of how to store state on each user
 function count(session) {
-  let count = (session.get('count') || 0) + 1
-  session.set('count', count)
-  sendMessage(session, `${count}`)
+    let count = (session.get('count') || 0) + 1
+    session.set('count', count)
+    sendMessage(session, `${count}`)
+}
+
+function currentBills(session) {
+    sendMessage(session,'1. bakchod billi , 2.ndjasnddkj')
+
 }
 
 function donate(session) {
-  // request $1 USD at current exchange rates
-  Fiat.fetch().then((toEth) => {
-    session.requestEth(toEth.USD(1))
-  })
+    // request $1 USD at current exchange rates
+    Fiat.fetch().then((toEth) => {
+        session.requestEth(toEth.USD(1))
+})
 }
 
-// HELPERS
+// HELPERS  
 
 function sendMessage(session, message) {
-  let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
-  ]
-  session.reply(SOFA.Message({
-    body: message,
-    controls: controls,
-    showKeyboard: false,
-  }))
+    let controls = [
+        {type: 'button', label: 'Current Bills', value: 'currentBills'},
+        {type: 'button', label: 'Count', value: 'count'},
+        {type: 'button', label: 'Donate', value: 'donate'},
+        {type: 'button', label: 'Values', value: 'showValues'}
+    ]
+    session.reply(SOFA.Message({
+        body: message,
+        controls: controls,
+        showKeyboard: false,
+    }))
 }
